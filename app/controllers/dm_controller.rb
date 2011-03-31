0ab79ed18635ecbde000de9c71a2d0bad936a228
Kyle d'Oliveira <kdoliveira@bravenet.com>
2011-03-30 23:42:33 -0700

    Subnav working

--- app/controllers/dm_controller.rb
+++ app/controllers/dm_controller.rb
@@ -3,17 +3,19 @@ class DmController < ApplicationController
   before_filter do
     @active_nav = "dm"
     @subnav = {
-      "Classes" => "",
+      "Classes" => {:action => :classes},
       "Spells" => "",
       "Feats" => "",
       "Races" => ""
     }
   end
 
-
-
   def index
 
+  end
+
+  def classes
+    @active_subnav = :classes
   end
 
 end
